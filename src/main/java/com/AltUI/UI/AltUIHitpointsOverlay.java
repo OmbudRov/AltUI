49cc1c9ba162afc978544d482643e4cd675afc98
OmbudRov <66506616+OmbudRov@users.noreply.github.com>
2022-08-17 15:28:42 +0530

    Create AltUIHitpointsOverlay.java

---
+++ src/main/java/com/AltUI/UI/AltUIHitpointsOverlay.java
@@ -0,0 +1,10 @@
+package com.AltUI.UI;
+
+import com.AltUI.AltUIPlugin;
+import net.runelite.client.ui.overlay.*;
+
+import javax.inject.Inject;
+import java.awt.*;
+
+public class AltUIHitpointsOverlay extends Overlay{
+}
